f44a59bb80d638a710ad0f02852140f519e8f20e
Yorth Ortegon <yorth21@gmail.com>
2024-03-14 22:16:09 -0500

    Add Next UI theme to content array and extend colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,18 @@ const { nextui } = require('@nextui-org/react')
 export default {
   content: [
     './index.html',
-    './src/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        udenar: {
+          primary: '#00923f',
+          secondary: '#F2FFE9'
+        }
+      }
+    }
   },
   darkMode: 'class',
   plugins: [nextui()]
